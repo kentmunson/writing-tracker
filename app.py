30ef2978f2ce7610fd7f60c26736e56c9424a46e
Kent Munson <kent.munson@gmail.com>
2024-12-30 18:04:43 -0800

    delete unused files

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 Offers Google Doc Analysis
 """
 import os
-import yaml
 
-from flask import Flask, render_template
+import yaml
+from flask import Flask
 
 from src.docs import get_word_counts
 
@@ -15,26 +15,23 @@ with open("config.yaml", "r") as f:
     CONFIG = yaml.safe_load(f)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     """Return a friendly HTTP greeting."""
-    message = "It's running!"
-
-    """Get Cloud Run environment variables."""
-    service = os.environ.get('K_SERVICE', 'Unknown service')
-    revision = os.environ.get('K_REVISION', 'Unknown revision')
-
-    return render_template('index.html',
-        message=message,
-        Service=service,
-        Revision=revision)
+    return {"status": "ok"}
 
 
 @app.route("/counts")
 def word_counts():
-    return get_word_counts(config=CONFIG)
+    """Get word counts for all documents in the app's config."""
+    word_counts = get_word_counts(config=CONFIG)
+    total = sum(list(word_counts.values()))
+    return {
+        "word_counts": word_counts,
+        "total": total,
+    }
 
 
-if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
+if __name__ == "__main__":
+    server_port = os.environ.get("PORT", "8080")
+    app.run(debug=False, port=server_port, host="0.0.0.0")
